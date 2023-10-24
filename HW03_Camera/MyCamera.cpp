2252b0dd41fbfdfcc90f29b294e6e903e62f5ec3
WillJerum <92756713+WillJerum@users.noreply.github.com>
2023-10-24 17:12:59 -0400

    Provisionally completed A03

--- HW03_Camera/MyCamera.cpp
+++ HW03_Camera/MyCamera.cpp
@@ -5,7 +5,10 @@ void MyCamera::SetPositionTargetAndUpward(vector3 a_v3Position, vector3 a_v3Targ
 {
 	//TODO:: replace the super call with your functionality
 	//Tip: Changing any positional vector forces you to calculate new directional ones
-	super::SetPositionTargetAndUpward(a_v3Position, a_v3Target, a_v3Upward);
+	
+	m_v3Position = a_v3Position;
+	m_v3Target = a_v3Target;
+	m_v3Upward = a_v3Upward;
 
 	//After changing any vectors you need to recalculate the MyCamera View matrix.
 	//While this is executed within the parent call above, when you remove that line
@@ -21,16 +24,24 @@ void MyCamera::MoveForward(float a_fDistance)
 	//		 in the _Binary folder you will notice that we are moving 
 	//		 backwards and we never get closer to the plane as we should 
 	//		 because as we are looking directly at it.
-	m_v3Position += vector3(0.0f, 0.0f, a_fDistance);
-	m_v3Target += vector3(0.0f, 0.0f, a_fDistance);
+
+	// Calculate position and target
+	m_v3Position += m_v3Forward * a_fDistance;
+	m_v3Target += m_v3Forward * a_fDistance;
 }
 void MyCamera::MoveVertical(float a_fDistance)
 {
 	//Tip:: Look at MoveForward
+
+	// Calculate position and target
+	m_v3Position += m_v3Upward * a_fDistance;
+	m_v3Target += m_v3Upward * a_fDistance;
 }
 void MyCamera::MoveSideways(float a_fDistance)
 {
 	//Tip:: Look at MoveForward
+	m_v3Position += m_v3Rightward * a_fDistance;
+	m_v3Target += m_v3Rightward * a_fDistance;
 }
 void MyCamera::CalculateView(void)
 {
@@ -40,6 +51,21 @@ void MyCamera::CalculateView(void)
 	//		 it will receive information from the main code on how much these orientations
 	//		 have change so you only need to focus on the directional and positional 
 	//		 vectors. There is no need to calculate any right click process or connections.
+
+	// Create individual quaternions for each rotation
+	glm::quat rotationPitch = glm::angleAxis(m_v3PitchYawRoll.x, glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::quat rotationYaw = glm::angleAxis(m_v3PitchYawRoll.y, glm::vec3(0.0f, 1.0f, 0.0f));
+
+	// Combine the quaternions
+	glm::quat combinedRotation = rotationYaw * rotationPitch;
+
+	// Calculate the camera's new forward, target, up, and rightward vectors
+	m_v3Forward = glm::normalize(glm::rotate(combinedRotation, glm::vec3(0.0f, 0.0f, -1.0f)));
+	m_v3Target = m_v3Position + m_v3Forward;
+	m_v3Upward = glm::normalize(glm::rotate(combinedRotation, glm::vec3(0.0f, 1.0f, 0.0f)));
+	m_v3Rightward = glm::normalize(glm::cross(m_v3Forward, m_v3Upward));
+
+	// Calculate the view matrix
 	m_m4View = glm::lookAt(m_v3Position, m_v3Target, m_v3Upward);
 }
 //You can assume that the code below does not need changes unless you expand the functionality
